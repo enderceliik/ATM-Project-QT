fa77ef32d3d07e0cfb8e17aff6eabf6e8821cdae
Ender ÇELİK <celikendertayyip@gmail.com>
2023-07-21 14:04:06 +0300

    TCP communication will be implemented

--- ServerSide/tcpserver.cpp
+++ ServerSide/tcpserver.cpp
@@ -26,24 +26,39 @@ void TcpServer::onReadyRead()
 {
     tcpSocket = qobject_cast<QTcpSocket*>(sender());
     QString receiveData = QString(tcpSocket->readAll());
+    QJsonObject jsonObject;
     if(receiveData.contains("getUsername"))
     {
         qDebug() << "sql query...";
-        QJsonObject jsonObject;
+
         jsonObject["response"] = "true";
-        jsonObject["userID"] = "61612929";
-        QJsonDocument jsonDocument(jsonObject);
-        QByteArray jsonDataByteArray = jsonDocument.toJson();
-        tcpSocket->write(jsonDataByteArray);
-        if(tcpSocket->waitForBytesWritten(2000))
-        {
-            qDebug() << "Data send to client";
-        } else
-        {
-            qDebug() << "Data didn't send to client";
-        }
+        jsonObject["userID"] = 1;
+        jsonObject["name"] = "Ender";
+        jsonObject["surname"] = "Celik";
+//        jsonObject["balance"] = 16.654;
+//        jsonObject["Iban"] = "296129";
+//        jsonObject["userType"] = 0;
 
     }
-
-
+    else if (receiveData.contains("controlPassword"))
+    {
+        qDebug() << "sql query... password";
+        jsonObject["response"] = "true";
+        jsonObject["userID"] = 1;
+        jsonObject["name"] = "Ender";
+        jsonObject["surname"] = "Celik";
+        jsonObject["balance"] = 16.654;
+        jsonObject["Iban"] = "296129";
+        jsonObject["userType"] = 0;
+    }
+    QJsonDocument jsonDocument(jsonObject);
+    QByteArray jsonDataByteArray = jsonDocument.toJson();
+    tcpSocket->write(jsonDataByteArray);
+    if(tcpSocket->waitForBytesWritten(2000))
+    {
+        qDebug() << "Data send to client";
+    } else
+    {
+        qDebug() << "Data didn't send to client";
+    }
 }
